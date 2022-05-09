8d1b457b627a9de372ad17ac3b6bb5eb4e5b525d
Antony013 <83763776+Antony013@users.noreply.github.com>
2022-05-09 13:48:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+// 	$("#form-register").hide();
+// 	$("#box-co").hide();
+// });
+
+// $(function() {
+// 	$(".btn-register").click(function(){
+// 		$("#form-login").hide();
+// 		$("#form-register").show();
+// 		$("#box-reg").hide();
+// 		$("#box-co").show();
+// 	});
+// 	$(".btn-log").click(function(){
+// 		$("#form-login").show();
+// 		$("#form-register").hide();
+// 		$("#box-reg").show();
+// 		$("#box-co").hide();
+// 	});
+// });
