d2fae6179181bb26ba792427b2b613423c6f0e32
Maxime Batista <90443919+BatMaxou@users.noreply.github.com>
2022-05-23 14:53:25 +0200

    Update footer.php

--- projet_fin_annee_sandwich/require/header_footer/footer.php
+++ projet_fin_annee_sandwich/require/header_footer/footer.php
@@ -1,8 +1,35 @@
 <footer>
-	<div id="conteneur-footer">
-		<div>Contenu</div>
+	<div id="footer">
+		<div>
+			<!-- <h5>
+				LycÃ©e Saint-Vincent
+			<br>
+				ENSEIGNEMENT SECONDAIRE & SUPÃ‰RIEUR
+			</h5> -->
+			<p>
+				<!-- 30 rue de Meaux, 60300 Senlis
+			<br>
+				<i class="bi bi-telephone"></i> 03 44 53 96 40
+			<br> -->
+				Â© Tous droits rÃ©servÃ©s
+			</p>
+		</div>
+		<!-- <div>
+			<h5>Menu</h5>
+			<ul>
+				<li>
+					<a href="index.php">Accueil</a>
+				</li>
+				<li>
+					<a href="require/login/login.php">Se connecter</a>
+				</li>
+				<li>
+					<a href="require/register/register.php">S'inscrire</a>
+				</li>
+				<li>
+					<a href="require/login/login_backoffice.php">Backoffice</a>
+				</li>
+			</ul>
+		</div> -->
 	</div>
 </footer>
-
-</body>
-</html>
